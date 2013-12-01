f7cc600b2837d6cdce193294bdbcc25bb500a890
Omar Abou Mrad <omar.aboumrad@gmail.com>
2013-12-01 14:47:39 +0200

    added xdefaults

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -34,9 +34,9 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
--- beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 local configdir = awful.util.getdir ("config")
-beautiful.init (configdir .. "/theme.lua")
+-- beautiful.init (configdir .. "/theme.lua")
+beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
